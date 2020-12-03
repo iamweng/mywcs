adfb24f4e6e9848fe3e1be2f067d0f5db06b0530
iamweng <2230001619@qq.com>
2020-12-03 20:18:10 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,6 +3,7 @@
         <!-- Header -->
         <el-header>
             <span>WCS</span>
+            <h3>您好，{{ user.username }}</h3>
             <el-button type="info" @click="userLogout">退出</el-button>
         </el-header>
         <el-container>
@@ -32,13 +33,21 @@
     export default {
         data(){
             return{
+                user: {},
                 menuList: [],
                 iconList: ["el-icon-user","el-icon-set-up","el-icon-goods","el-icon-s-order","el-icon-s-data","el-icon-s-data"],
                 isCollapse: false,
             }
         },
-        created(){this.getMenuList()},
+        created(){this.getMyselfInfo()},
         methods: {
+			async getMyselfInfo() {
+		        const result = await this.$http.get('/info')
+                if(result.data.code !== 200) return this.$message.error(result.data.msg)
+				this.$message.success(result.data.msg)
+				this.user = result.data.data
+                this.getMenuList()
+			},
             userLogout:function() {
                 window.sessionStorage.clear()
                 this.$router.push('/login')
